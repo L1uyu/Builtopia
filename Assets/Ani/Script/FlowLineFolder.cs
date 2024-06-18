fd1d51cb4e076b09483ec3999344cd89092d9899
MOYA\clothes <realzihan@foxmail.com>
2024-06-18 17:18:47 +0800

    Some useless change

--- Assets/Ani/Script/FlowLineFolder.cs
+++ Assets/Ani/Script/FlowLineFolder.cs
@@ -9,11 +9,21 @@ public class FlowLineFolder : AnimateObject
     {
         base.EachDeltaAnimation(elapsedTime);
         float ease_in = elapsedTime < ease_time ? GetEaseAlpha(elapsedTime / ease_time) : 1.0f;
-        //float ease_out = 1.0f;//duration - elapsedTime < ease_time ? (duration - elapsedTime) / ease_time : 1.0f;
+        float ease_out = duration - elapsedTime < ease_time ? (duration - elapsedTime) / ease_time : 1.0f;
         foreach (GameObject obj in underObjects)
         {
             Material material = obj.GetComponent<LineRenderer>().material;
-            material.SetFloat("_Alpha", ease_in);
+            material.SetFloat("_Alpha", ease_in * ease_out);
+        }
+    }
+
+    public override void EndAnimation()
+    {
+        base.EndAnimation();
+        foreach (GameObject obj in underObjects)
+        {
+            Material material = obj.GetComponent<LineRenderer>().material;
+            material.SetFloat("_Alpha", 0.0f);
         }
     }
 
--- Assets/Ani/Script/UIFolder.cs
+++ Assets/Ani/Script/UIFolder.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class UIFolder : AnimateObject
 {
-    public Transform target; // The target to look at
-    public float rotationSpeed = 5f; // Speed of the rotation
+    
     private DancingNumber dancingNumber;
 
-    private void Start()
-    {
-        target = Camera.main.transform;
-    }
 
     public override void BeginAnimation()
     {
@@ -26,21 +21,5 @@ public class UIFolder : AnimateObject
             dancingNumber.StartUpdateNumber();
         }
     }
-    void Update()
-    {
-        if (target != null)
-        {
-            // Update pivot position if needed
-            // Example: pivotPosition = some value or calculation;
 
-            // Calculate the direction to the target
-            Vector3 direction = (transform.position - target.position).normalized;
-
-            // Calculate the desired rotation
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-
-            // Smoothly rotate towards the target
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
-        }
-    }
 }
